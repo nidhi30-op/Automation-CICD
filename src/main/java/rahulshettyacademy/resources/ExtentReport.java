aa04c06f08e72fc08f7ca4cbc59f28db7594c4da
nidhi30-op <nidhitest50@gmail.com>
2025-05-17 23:52:39 +0530

    Update ExtentReport.java

--- src/main/java/rahulshettyacademy/resources/ExtentReport.java
+++ src/main/java/rahulshettyacademy/resources/ExtentReport.java
@@ -1,9 +1,11 @@
 package rahulshettyacademy.resources;
+package org.testng.annotations
 
 import org.testng.annotations.BeforeTest;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 
 public class ExtentReport {
 
